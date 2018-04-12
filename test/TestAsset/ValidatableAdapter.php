94ea7a911408ecce8f3889c929667912b99f79db
Matthew Weier O'Phinney <matthew@zend.com>
2018-04-12 15:12:57 -0500

    CS issues with ValidatableAdapter test asset
    - license docblock
    - Adds docblock to authenticate() method)
    - makes property private

--- test/TestAsset/ValidatableAdapter.php
+++ test/TestAsset/ValidatableAdapter.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-authentication for the canonical source repository
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-authentication/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\Authentication\TestAsset;
@@ -17,7 +15,7 @@ class ValidatableAdapter extends AuthenticationAdapter
     /**
      * @var int Authentication result code
      */
-    protected $code;
+    private $code;
 
     /**
      * @param int $code
@@ -27,6 +25,9 @@ class ValidatableAdapter extends AuthenticationAdapter
         $this->code = $code;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function authenticate()
     {
         return new AuthenticationResult($this->code, 'someIdentity');
